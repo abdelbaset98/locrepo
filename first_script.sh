3d148c863982f44bf7a3b7ce329123a757f19ded
ahmed abdl bast <eng.ahmed.cui@gmail.com>
2026-03-13 05:34:54 -0400

    a mod in first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
+function is_int(){
+	echo $i
+ if [[ "$i" =~ ^[0-9]+$ ]];
+	then 
+		return 0
+	else 
+		return 1	
+ fi
+}
 
-echo "the first bash script in github"
+
+
+
+until [[ ${i,,} = 'khalas' ]];
+do
+
+read -p "enter a number: " i
+
+if is_int;
+then
+	if (( i % 2 == 0 ));
+	then
+		echo "$i is even"
+	else
+		echo "$i is odd"
+	fi
+else
+	echo "Enter a valid number not a string"
+
+fi
+
+done
